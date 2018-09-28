b464dda926e1de90e7f7db61bb4b9085a397d3f4
Palash83 <pgp18palashjain@imt.ac.in>
2018-09-28 19:41:35 +0530

    a

--- file1.R
+++ file1.R
@@ -1,1 +1,5 @@
 women
+git config --global user.email "pgp18palashjain@imt.ac.in"
+git config --global user.name "Palash83"
+
+mtcars
---
+++ file2.R
@@ -0,0 +1,19 @@
+# Regression Analysis
+data(women)
+women
+names(women)
+str(women)
+cov(women$height, women$weight)
+cor(women$height, women$weight)
+plot(x=women$height, y=women$weight)
+abline(lm(weight ~ height, data = women), col = 'red')
+names(women) #x-IV y-DV
+fit1 = lm(weight ~ height, data = women) #creating a Model
+summary(fit1) #summary of Model
+attributes(fit1)
+coef(fit1)
+range(women$height)
+(new1 = data.frame(height = c(65,66)))
+new1
+(p1=predict(fit1, newdata = new1))
+cbind(new1,p1)
